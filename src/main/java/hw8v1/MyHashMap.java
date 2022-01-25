5587aad3b4c7e9956913cee6755828a74316c802
SergeyHodak <4Kserge4Kserge4K@gmail.com>
2022-01-25 14:09:29 +0200

    теперь только тот тип который указали. и добавлено сжатие коллекции относительно заполненности. +константы класса

--- src/main/java/hw8v1/MyHashMap.java
+++ src/main/java/hw8v1/MyHashMap.java
@@ -18,7 +18,9 @@ package hw8v1;
 //TODO видеоролик, который помог понять, чего нужно достичь, при решении этой задачи https://youtu.be/DpHI6yRT6Es
 
 public class MyHashMap<K, V> {
-    private Node[] table = new Node[16]; // массив, по умолчанию из 16 пустых нодов
+    private Node<K, V>[] table = new Node[16]; // массив, по умолчанию из 16 пустых нодов
+    private static final int BASKET_SIZE = 8; // базовое значение объема корзин
+    private static final float FULLNESS = 75f; // заполненность, барьер для сжатия и расширений количества корзин в массиве
 
     static class Node<K, V> { // клас создающий ноды
         final int hash; // хеш текущего элемента
@@ -37,15 +39,15 @@ public class MyHashMap<K, V> {
         public final void setValue(V newValue) {value = newValue;} // установить новое значение
     }
 
-    public void put(Object key, Object value) { //добавляет пару ключ + значение
+    public void put(K key, V value) { //добавляет пару ключ + значение
         int h; // изначально хеш пусть будет пустым, по дефолту нуль
         int hash = (key == null) ? 0 : (h = key.hashCode()) ^ (h >>> 16); // если ключ не нул, воспользуемся стандартным хешь методом
         int n = table.length; // размер массива
         int buckets = hash % n; // получить индекс корзины для данных
         if (table[buckets] == null) { // в этой позиции пустая нода
-            table[buckets] = new Node<Object, Object>(hash, key, value, null); // перезаписываем ее на наши данные
+            table[buckets] = new Node<K, V>(hash, key, value, null); // перезаписываем ее на наши данные
         } else { //иначе, в позиции уже есть минимум одна нода
-            Node nexts = table[buckets]; //временная нода
+            Node<K, V> nexts = table[buckets]; //временная нода
             int count = 0; // счетчик сколько нодов в данной корзине
             while (nexts.next != null) { // выполнять пока ссылка на следующую ноду не пуста
                 if (nexts.key == key) {break;} // если нода с таким ключем, уже есть
@@ -55,22 +57,24 @@ public class MyHashMap<K, V> {
             if (nexts.key == key) { // если нода с таким ключем, уже есть
                 nexts.setValue(value); // перезаписать в нее значение с добавляемой
             } else { // иначе, такого ключа еще не было
-                nexts.next = new Node<Object, Object>(hash, key, value, null); // создаем новую ноду, и отправляем на нее ссылку
+                nexts.next = new Node<K, V>(hash, key, value, null); // создаем новую ноду, и отправляем на нее ссылку
             }
 
-            if(count == 8){ // если в затронутой корзине уже 8 нодов
-                collectionExpansion(); // расширить коллекцию
+            if(count == BASKET_SIZE || 100f/(table.length * BASKET_SIZE) * size() >= FULLNESS){ // если в затронутой корзине уже 8 нодов, или коллекция заполнена на 75%+
+                collectionExpansion(table.length * 2); // расширить коллекцию в 2 раза
             }
         }
     }
 
-    private void collectionExpansion() { // расширение коллекции
-        Node[] old = table; // сохранить старую коллекцию
-        table =  new Node[table.length * 2]; // создать новую пустую в два раза больше коллекцию
+    private void collectionExpansion(int novaia) { // изменение количества корзин в коллекции
+        Node<K, V>[] old = table; // сохранить старую коллекцию
+        table =  new Node[novaia]; // создать новую пустую в два раза больше коллекцию
         //записать в новую коллекцию старую коллекцию
-        for (int i = 0; i < old.length; i++) { // пробежка по старой коллекции
-            if (old[i] == null) {continue;} //если нет ноды в позиции, перейти на следующую позицию
-            Node nexts = old[i]; //временная нода
+        for (Node<K, V> kvNode : old) { // пробежка корзинам в старой коллекции
+            if (kvNode == null) { // если корзина пуста
+                continue; // перейти в следующую корзину
+            }
+            Node<K, V> nexts = kvNode; //временная нода
             put(nexts.getKey(), nexts.getValue()); //добавление ноды в новую коллекцию
             while (nexts.next != null) { //пока есть ссылка на следующую ноду
                 nexts = nexts.next; // записать следующую ноду
@@ -82,9 +86,11 @@ public class MyHashMap<K, V> {
     @Override //метка переопределения
     public String toString() { // посмотреть коллекцию мапы
         StringBuilder result = new StringBuilder("["); // для быстрой конкатенации стрингбилдер
-        for (int i = 0; i < table.length; i++) { // пробежка мо массиву нод
-            if (table[i] == null) {continue;} //если нет ноды в позиции
-            Node nexts = table[i]; //временная нода
+        for (Node<K, V> node : table) { // пробежка мо массиву нод
+            if (node == null) {
+                continue;
+            } //если нет ноды в позиции
+            Node<K, V> nexts = node; //временная нода
             result.append(nexts).append(", "); // внести ноду в результат
             while (nexts.next != null) { //пока есть ссылка на следующую ноду
                 nexts = nexts.next; // записать следующую ноду
@@ -100,8 +106,7 @@ public class MyHashMap<K, V> {
         return res; //возвращаем убрав два последних символа
     }
 
-    public void remove(Object key) throws KeyNotFoundException { // удаляет пару по ключу
-        //TODO еще бы добавить обработку по уменьшению корзин в зависимости от заполненности, если ниже 50%
+    public void remove(K key) throws KeyNotFoundException { // удаляет пару по ключу
         int h; // изначально хеш пусть будет пустым, по дефолту нуль
         int hash = (key == null) ? 0 : (h = key.hashCode()) ^ (h >>> 16); // если ключ не нул, воспользуемся стандартным хешь методом
         int n = table.length; // размер массива
@@ -109,10 +114,13 @@ public class MyHashMap<K, V> {
         if (table[buckets] == null) { // ели корзина пуста
             throw new KeyNotFoundException("Такого ключа нет в коллекции"); //вызвать исключение, и выдать такой текст
         }
-        Node removable = table[buckets]; // корзина в которой нужно поискать удаляемого
-        Node previousNode = null; // хранилище для предыдущей ноды
+        Node<K, V> removable = table[buckets]; // корзина в которой нужно поискать удаляемого
+        Node<K, V> previousNode = null; // хранилище для предыдущей ноды
         if (removable.getKey() == key) { // если ключ ноды совпадает с удаляемым
-            table[buckets] = removable.next; // шапка корзины станет не первый, а второй элемент
+            table[buckets] = removable.next; // шапкой корзины станет не первый, а второй элемент
+            if(100f/(table.length * BASKET_SIZE) * size() < FULLNESS){ // если коллекция заполнена на x < 75%
+                collectionExpansion(table.length / 2); // сжать коллекцию в 2 раза
+            }
             return; // прекратить выполнение метода
         }
         while (removable.next != null) { // выполнять пока ссылка не пустая
@@ -120,6 +128,9 @@ public class MyHashMap<K, V> {
             removable = removable.next; // перешагнуть на следующую ноду
             if (removable.getKey() == key) { // если ключ ноды совпадает с удаляемым
                 previousNode.next = removable.next; // в предыдущей ноде изменить ссылку через одну ноду
+                if(100f/(table.length * BASKET_SIZE) * size() < FULLNESS){ // если коллекция заполнена на x < 75%
+                    collectionExpansion(table.length / 2); // сжать коллекцию в 2 раза
+                }
                 return; // прекратить выполнение метода
             }
         }
@@ -138,9 +149,11 @@ public class MyHashMap<K, V> {
 
     public int size() { // возвращает размер коллекции
         int result = 0; // счетчик нод
-        for (int i = 0; i < table.length; i++) { // пробежка мо массиву нод
-            if (table[i] == null) {continue;} //если нет ноды в позиции
-            Node nexts = table[i]; //временная нода
+        for (Node<K, V> node : table) { // пробежка мо массиву нод
+            if (node == null) {
+                continue;
+            } //если нет ноды в позиции
+            Node<K, V> nexts = node; //временная нода
             result++; // внести ноду в результат
             while (nexts.next != null) { //пока есть ссылка на следующую ноду
                 nexts = nexts.next; // записать следующую ноду
@@ -150,7 +163,7 @@ public class MyHashMap<K, V> {
         return result; //возвращаем убрав два последних символа
     }
 
-    public Object get(Object key) throws KeyNotFoundException { // возвращает значение(Object value) по ключу
+    public V get(K key) throws KeyNotFoundException { // возвращает значение(Object value) по ключу
         int h; // изначально хеш пусть будет пустым, по дефолту нуль
         int hash = (key == null) ? 0 : (h = key.hashCode()) ^ (h >>> 16); // если ключ не нул, воспользуемся стандартным хешь методом
         int n = table.length; // размер массива
@@ -158,7 +171,7 @@ public class MyHashMap<K, V> {
         if (table[buckets] == null) { // ели корзина пуста
             throw new KeyNotFoundException("Такого ключа нет в коллекции"); //вызвать исключение, и выдать такой текст
         }
-        Node result = table[buckets]; // корзина в которой нужно поискать ключ для выдачи значения
+        Node<K, V> result = table[buckets]; // корзина в которой нужно поискать ключ для выдачи значения
         if (result.getKey() == key) { // если ключ ноды совпадает с искомым
             return result.getValue(); // отдать результат
         }
@@ -174,6 +187,7 @@ public class MyHashMap<K, V> {
     public static void main(String[] args) throws KeyNotFoundException { // запускалка для тестов с интеграцией исключений
         MyHashMap<String, Integer> test = new MyHashMap<>(); // создать экземпляр коллекции мап
         System.out.println(test); // посмотреть на пустую коллекцию
+        //test.put(1, 1); //не пропустит, такое добавление, тип не соответствует
         test.put("A", 1); test.put("B", 2); test.put("C", 3); test.put("D", 4); test.put("E", 5); test.put("F", 6);
         test.put("G", 7); test.put("H", 8); test.put("I", 9); test.put("J", 10); test.put("K", 11); test.put("L", 12);
         test.put("M", 13); test.put("N", 14); test.put("O", 15); test.put("P", 16); // заполнили коллекцию по 1му элементу
@@ -200,22 +214,14 @@ public class MyHashMap<K, V> {
 
         test.put("CA", 79); test.put("CB", 80); test.put("CC", 81); test.put("CD", 82); test.put("CE", 83); test.put("CF", 84);
         test.put("CG", 85); test.put("CH", 86); test.put("CI", 87); test.put("CJ", 88); test.put("CK", 89); test.put("CL", 90);
-        test.put("CM", 91); test.put("CN", 92); test.put("CO", 93); test.put("CP", 94); test.put("CQ", 95); test.put("CR", 96);
-        test.put("CS", 97); test.put("CT", 98); test.put("CU", 99); test.put("CV", 100); test.put("CW", 101); test.put("CX", 102);
-        test.put("CY", 103); test.put("CZ", 104);
-
-        test.put("DA", 105); test.put("DB", 106); test.put("DC", 107); test.put("DD", 108); test.put("DE", 109); test.put("DF", 110);
-        test.put("DG", 111); test.put("DH", 112); test.put("DI", 113); test.put("DJ", 114); test.put("DK", 115); test.put("DL", 116);
-        test.put("DM", 117); test.put("DN", 118); test.put("DO", 119); test.put("DP", 120); test.put("DQ", 121); test.put("DR", 122);
-        test.put("DS", 123); test.put("DT", 124);
-
+        test.put("CM", 91); test.put("CN", 92); test.put("CO", 93); test.put("CP", 94); test.put("CQ", 95);
         System.out.println(test); // глянуть на коллекцию
-        test.put("DU", 125); // эта строчка вызовет расширение коллекции
+        test.put("CR", 96);// вызовет расширение коллекции по % заполненности всей коллекции
         System.out.println(test); // глянуть на коллекцию, расширилась и отсортировалась по новому
 
         System.out.println("----------------------------------------------"); //разделитель между тестами
 
-        test.remove("DU"); // убрать ноду по ключу
+        test.remove("CQ"); // убрать ноду по ключу
         System.out.println(test); // глянуть на коллекцию
 
         System.out.println("----------------------------------------------"); //разделитель между тестами
